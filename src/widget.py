daef415d1b271368eb1b7d08dab5e6348eff4959
Emil <kitanarau@yandex.ru>
2025-11-04 20:08:28 +0500

    add get data

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,4 @@
+from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(user_input: str) -> str:
     """Функция, скрывающая номер или счёт карты"""
     input_parts = user_input.split()
@@ -11,9 +12,16 @@ def mask_account_card(user_input: str) -> str:
     str_card_name = " ".join(card_name)
     str_card_number = " ".join(card_number)
     if len(str_card_number) > 16:
-        answer = str_card_name + " " + "**" + str_card_number[-4:]
+        answer = str_card_name + " " + get_mask_account(int(str_card_number))
     else:
-        answer = str_card_name + " " + str_card_number[0:4] + " " + str_card_number[
-                                                                        4:6] + "** **** " + str_card_number[-4:]
+        answer = str_card_name + " " + get_mask_card_number(int(str_card_number))
 
     return answer
+
+def get_date(iso_format_date: str) -> str:
+    """Функция, преобразующая строку с датой из одного формата в другой"""
+    new_format_date = iso_format_date[8:10]
+    new_format_month = iso_format_date[5:7]
+    new_format_year = iso_format_date[0:4]
+    required_date_format = new_format_date + "." + new_format_month + "." + new_format_year
+    return required_date_format
