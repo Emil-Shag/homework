deaa76370ce7fbbe83448fb3f0c236a4a3b6fc2f
Emil <kitanarau@yandex.ru>
2025-11-05 17:11:13 +0500

    homework_10_1 done

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,7 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция, которая частично скрывает номер карты."""
     str_card_number = str(card_number)
-    mask_card_number = (
-        str_card_number[0:4]
-        + " "
-        + str_card_number[4:6]
-        + "** **** "
-        + str_card_number[-4:]
-    )
+    mask_card_number = str_card_number[0:4] + " " + str_card_number[4:6] + "** **** " + str_card_number[-4:]
     return mask_card_number
 
 
---
+++ src/processing.py
@@ -0,0 +1,13 @@
+def filter_by_state(dicts_list: list[dict], state_filter: str="EXECUTED") -> list[dict]:
+    """Функция, сортриующая словари по ключу state."""
+    sorted_list = []
+    for operation in dicts_list:
+        if operation["state"] == state_filter:
+            sorted_list.append(operation)
+    return sorted_list
+
+
+def sort_by_date(dicts_list: list[dict], descending_sort: bool=True) -> list[dict]:
+    """Функция, сортируующая словари по дате"""
+    sorted_by_date = sorted(dicts_list, key=lambda operation: operation["date"], reverse=descending_sort)
+    return sorted_by_date
