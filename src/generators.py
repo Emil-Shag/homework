1c98c07b698c145410574dda6e637a53e8d6fba8
Emil <kitanarau@yandex.ru>
2025-12-01 19:26:29 +0500

    homework_11_1 start

--- src/generators.py
+++ src/generators.py
@@ -1,15 +1,18 @@
-def filter_by_currency(transactions: list[dict], currency: str) -> dict:
+from typing import Iterator, Generator
+
+
+def filter_by_currency(transactions: list[dict], currency: str) -> Iterator:
     """Генератор, который фильтрует транзакции по заданной валюте"""
     for transaction in transactions:
-        if transaction.get("operationAmount").get("currency").get("code") == currency:
+        if transaction.get("operationAmount", {}).get("currency", {}).get("code") == currency:
             yield transaction
 
-def transaction_descriptions(transactions: list[dict]) -> str:
+def transaction_descriptions(transactions: list[dict]) -> Iterator:
     """Генератор, принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
     for transaction in transactions:
         yield transaction.get("description")
 
-def card_number_generator(start_value: int, finish_value: int) -> str:
+def card_number_generator(start_value: int, finish_value: int) -> Generator:
     """Генератор, который выдает номера банковских карт"""
     for i in range(start_value, finish_value + 1):
         zero_amount = 16 - len(str(i))
